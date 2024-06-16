d5c27aadf67953ce9819d1ba58414a1010adf5b7
Suryanshh12 <suryansh.kr1212@gmail.com>
2024-06-16 15:57:18 +0530

    Create sum1.java
    sum of 2 numbers

---
+++ sum1.java
@@ -0,0 +1,9 @@
+class Sum{
+        public static void main(String[] args) {
+                int a=1;
+                int b=2;
+
+                int sum=a+b;
+                System.out.println(sum);
+        }
+}
